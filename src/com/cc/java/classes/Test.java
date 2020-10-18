c93357cc47e5d7663847054bd248d2f77fd1fba9
Peter Schmidt <schmidt.multimedia@gmail.com>
2020-10-19 01:32:58 +0200

    Add files via upload

---
+++ src/com/cc/java/classes/Test.java
@@ -0,0 +1,10 @@
+package com.cc.java.classes;
+
+public class Test {
+
+	public void sayHello() {
+		System.out.println("Hi");
+	}
+	
+	
+}
---
+++ src/com/cc/java/main/Start.java
@@ -0,0 +1,18 @@
+package com.cc.java.main;
+
+import com.cc.java.classes.Test;
+
+public class Start {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Test test = new Test();
+		test.sayHello();
+		
+		//com.cc.schmidt.classes.Test test = new com.cc.schmidt.classes.Test();
+		//test.sayHello();
+		
+	}
+
+}
